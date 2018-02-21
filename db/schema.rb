b7a26296c65bcfbcf26af7baa770da8849f6d72a
aciukurescu <andrewciukurescu@gmail.com>
2018-02-21 16:20:13 -0500

    created users table and added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221200956) do
+ActiveRecord::Schema.define(version: 20180221211508) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
